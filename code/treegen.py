34987360a88cf26b0f52603a8212b1042a106bed
Mrinal Dhar <mrinal.dhar@gmail.com>
2015-09-27 01:11:26 +0530

    Tree generation code added

--- code/treegen.py
+++ code/treegen.py
@@ -9,17 +9,16 @@ import sys
 class Node(object):
 	def __init__(self):
 		self.deps = []
-		self.parentNode = []
 		self.value = []
 		self.ID = 0
 		self.parentID = 0
 		self.parentREL = 0
 
+	def __lt__(self, other):
+		return self.ID < other.ID
+
 	def addNewDep(self, depNode):
 		self.deps.append(depNode)
-
-	def childOf(self, parent):
-		self.parentNode = parent
 
 def print_data(sents):
 	for sent in sents:
@@ -33,14 +32,13 @@ def print_tree(node, level):
 		print_tree(each, level+1)
 
 def putInPlace(root, node):
-#	print "Trying for ", node.value[1]
-#	print "Comparing ", node.parentID, " + ", root.ID
 	if node.parentID == root.ID:
-#		print "Placed:: ", node.value[1], " under ", root.value[1]
 		root.addNewDep(node)
 		return True
 	for each in root.deps:
-		putInPlace(each, node)
+		success = putInPlace(each, node)
+		if success == True:
+			return True
 
 def makeTree(sent):
 	nodes = {}
@@ -58,16 +56,12 @@ def makeTree(sent):
 			nodes[node.parentID].addNewDep(node)
 		else:
 			nodes[node.ID] = node
-#	print "Phase 1:"
-#	print nodes
-#	for each in nodes.keys():
-#		print_tree(nodes[each], 0)
 	x = len(nodes.keys())
 	while (x > 1):
 		deletion = []
 		for each in nodes.keys():
 			for every in nodes.keys():
-				if each!=every:
+				if each!=every and each!=rootNode:
 					success = putInPlace(nodes[every], nodes[each])
 					if success == True:
 						deletion.append(each)
@@ -75,13 +69,9 @@ def makeTree(sent):
 		for each in deletion:
 			del nodes[each]
 		x = len(nodes.keys())
-#	print "Nodes:", nodes
-	return nodes[rootNode]
-#	print "+"*50
-#	print "Phase 2:"
-#	print nodes
-#	for each in nodes.keys():
-#		return nodes[each]
+	for each in nodes.keys():
+		return nodes[each]
+
 def init(filename):
 	sep.init(filename)
 	sents = sep.sents_parts
@@ -92,6 +82,6 @@ def init(filename):
 		print_data([sent])
 		print_tree(tree, 0)
 		print "="*100
-	print trees
+
 if __name__=="__main__":
 	init(sys.argv[1])
