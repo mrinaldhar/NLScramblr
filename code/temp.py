034efe5f17c0f8415dc4799b02b265c73183383a
Chanakya <chanakya.malireddy@students.iiit.ac.in>
2015-09-30 21:44:33 +0530

    shuffle_vb added to permute verb position as well

--- code/temp.py
+++ code/temp.py
@@ -1,15 +1,58 @@
 from treegen import *
 from treeregen import *
+from scramblr import chunker, print_data, process_piped
+import separatr as sep
 import itertools
 
-trees = init('../data/ten_sample.conll')
+def print_sent(new_sent):
+	sep.sents = [new_sent]
+	sep.i = -1
+	sep.sents_parts = []
+	sep.breakdown()
+	sep.sents_parts[-1].pop()
+
+	sent = sep.sents_parts
+	sent = chunker(sent)[0]
+	count = 1
+	renumber = {'0': '0'}
+	ctype = {}
+	for k in xrange(len(sent)):
+		chunk = sent[k]
+		d = process_piped(chunk[0])
+		result = ''.join([i for i in d['chunkId'] if not i.isdigit()])
+		ctype[result] = ctype.get(result, 0) + 1
+		num = ctype[result]
+		if num == 1:
+			num = ''
+		else:
+			num = str(num)
+
+		for j in xrange(len(chunk)):
+			word = chunk[j]
+			if int(word[0]) != count:
+				renumber[word[0]] = str(count)
+			else:
+				renumber[word[0]] = word[0]
+			word[0] = str(count)
+			count += 1
+			d = process_piped(word)
+			d['chunkId'] = result + num
+			feats = [each+'-'+d[each] for each in d.keys()]
+			word[5] = '|'.join(feats)
+
+	
+	for k in xrange(len(sent)):
+		chunk = sent[k]
+		for j in xrange(len(chunk)):
+			word = chunk[j]
+			word[6] = renumber[word[6]]
+	print_data([sent])
+
+trees = init('../data/one_sample/one_sample.conll')
 responses = {}
-ofp = open('results', 'w')
-for tree in trees[3:4]:
+for tree in trees:
 	tree_sort(tree)
 	orig_sent = sent_gen(tree, '')
-	print
-	responses[orig_sent] = []
 	aux = [x for x in tree.deps if (x.parentREL.startswith('lwg') or x.parentREL == 'rsym')]
 	rest = [x for x in tree.deps if not (x.parentREL.startswith('lwg') or x.parentREL == 'rsym')]
 	rest = list(itertools.permutations(rest))
@@ -18,15 +61,10 @@ for tree in trees[3:4]:
 		each1 = list(each)
 		each1.extend(aux)
 		permutations.append(each1)
-	print len(permutations)
-	i = 1
+
 	for each in permutations:
 		tree.deps = each
-		new_sent= sent_gen(tree, '')
-		print i, new_sent
-		print
-		i += 1
-		reply = raw_input("Is it right? [0,1] ")
-		ofp.write(new_sent+"\t")
-		ofp.write(reply+"\n")
-		print "="*50
+		new_sents= shuffle_vb(tree)
+		#new_sents= [sent_gen(tree, '')]
+		for new_sent in new_sents:
+			print_sent(new_sent)
--- code/treeregen.py
+++ code/treeregen.py
@@ -1,4 +1,5 @@
-#! /usr/bin/python
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
 
 import operator
 
@@ -10,16 +11,48 @@ def sent_gen(root, sent):
 	'''
 
 	for dep in root.deps:
-		if not (dep.parentREL.startswith('lwg') or dep.parentREL == 'rsym'):
+		if not (dep.parentREL.startswith('lwg') or dep.parentREL.startswith('rsym')):
 			sent = sent_gen(dep, sent)
 
-	sent += root.value[1] + ' ' 
+	sent += '\t'.join(root.value) + '\n' 
 
 	for dep in root.deps:
-		if (dep.parentREL.startswith('lwg') or dep.parentREL == 'rsym'):
+		if (dep.parentREL.startswith('lwg') or dep.parentREL.startswith('rsym')):
 			sent = sent_gen(dep, sent)
 	
 	return sent
+
+def shuffle_vb(root):
+
+	'''
+	Given a dependency structure tree
+	regenerates several surface forms
+	by moving the verb.
+	'''
+
+	def make_sent(root, before, after, sent):
+		for dep in before:
+			sent = sent_gen(dep, sent)
+		sent += '\t'.join(root.value) + '\n'
+		for dep in root.deps:
+			if (dep.parentREL.startswith('lwg') or dep.parentREL.startswith('rsym') and dep.value[1] != '।'):
+				sent = sent_gen(dep, sent)
+		for dep in after:
+			sent = sent_gen(dep, sent)
+		for dep in root.deps:
+			if dep.value[1] == '।':
+				sent = sent_gen(dep, sent)
+		
+		return sent
+
+	sents = []
+	vb_args = [dep for dep in root.deps if not (dep.parentREL.startswith('lwg') or dep.parentREL.startswith('rsym'))]
+	for i in xrange(len(vb_args)+1):
+		before = vb_args[:i]
+		after = vb_args[i:]
+		sents.append(make_sent(root, before, after, ''))
+	return sents
+
 
 def tree_sort(root):
 
@@ -27,6 +60,7 @@ def tree_sort(root):
 	Given a dependency structure tree
 	sorts the branches by sentence id
 	'''
+
 	root.deps.sort(key=operator.attrgetter('ID'))
 	for dep in root.deps:
 		tree_sort(dep)
